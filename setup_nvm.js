151a6f8a4b6a896f828baa664312ac3b5032f37a
Mike Situ <msitu@splunk.com>
2017-08-21 15:03:43 -0700

    first commit

---
+++ setup_nvm.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const {exec, spawn} = require('child_process');
+const customNodeVersion = process.argv[2] || '';
+
+function getRecentNodeVersion() {
+    const versionPromise = new Promise((resolve, reject) => {
+        const remoteVersions = `source ~/.nvm/nvm.sh && nvm ls-remote ${customNodeVersion}`;
+        exec(remoteVersions, function(err, versionsString, stderr) {
+            if (stderr) {
+                reject(stderr);
+            }
+            const versionsArray = versionsString.split('\n');   
+            const recentVersion = versionsArray[versionsArray.length - 2].trim();                     
+            if (recentVersion !== 'N/A') {
+                resolve(recentVersion)
+            } else {
+                reject(`Error: ${customNodeVersion} not found!\nRun 'nvm ls-remote' to confirm your requested version exists.`)
+            }      
+        });
+    });
+    return versionPromise;
+}
+
+function createVsCodeConfig(version) {
+    const launchFilePath = '.vscode/launch.json';
+    const getNodePath = `
+    source ~/.nvm/nvm.sh > /dev/null
+    echo ${version} > .nvmrc
+    nvm use > /dev/null
+    which node
+    `;
+
+    const used = exec(getNodePath, function(err, localNodePath, stderr) {
+        if (stderr.includes('N/A: version')) {
+            console.error(stderr);
+            return;
+        }
+        
+        const launchFileTemplate = `{
+            "version": "0.2.0",
+            "configurations": [
+                {
+                    "type": "node",
+                    "request": "launch",
+                    "name": "Lanuch to Process",
+                    "program": "\$\{file\}",
+                    "protocol": "auto",
+                    "runtimeExecutable": "${localNodePath.trim()}"
+                }
+            ]
+        }`;
+
+        fs.writeFile(launchFilePath, launchFileTemplate, err => {
+            if (err) {
+                console.error(err);
+            }
+            console.log('created a new', launchFilePath);
+        });
+    });
+}
+
+const versionPromise = getRecentNodeVersion();
+versionPromise.then(version => {
+    createVsCodeConfig(version);
+}).catch(err => {
+    console.error(err);
+});
