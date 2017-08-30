d8265cd5dcb0e9e3d34cc93a31b42cfdc21c9ae5
Mike Situ <msitu@splunk.com>
2017-08-30 13:35:03 -0700

    catch promise rejection

--- setup_nvm.js
+++ setup_nvm.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
-const {
-    parseVersion,
-    getRecentNodeVersion,
-    createVsCodeConfig
-} = require('./config_factory');
+const { parseVersion, getRecentNodeVersion, createVsCodeConfig } = require('./config_factory');
 const customVersion = process.argv[2] || ''; // allows users to enter a specific node version
 
 getRecentNodeVersion(customVersion)
     .then(version => {
-        createVsCodeConfig(version).then(msg => console.log(msg));
+        createVsCodeConfig(version)
+            .then(msg => console.log(msg))
+            .catch(err => console.error(err));
     })
     .catch(err => {
         console.error(err);
